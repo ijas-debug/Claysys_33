3fc11e2dfd388fca68e887b48e98c8605e66e962
Ijas <118385369+ijas-debug@users.noreply.github.com>
2023-07-17 21:53:03 +0530

    Add files via upload

---
+++ Scripts/logout.js
@@ -0,0 +1,27 @@
+﻿window.onload = function () {
+    if (typeof history.pushState === "function") {
+        history.pushState(null, null, location.href);
+        window.onpopstate = function () {
+            history.go(1);
+        };
+    } else {
+        history.go(1);
+    }
+};
+
+function preventBack() {
+    window.history.forward();
+}
+ setTimeout("preventBack()", 0);
+window.onunloadfunction() {
+    null;
+}
+
+function preback() {
+    window.history.forward();
+}
+setTimeout("preback()", 0);
+window.onunload = function () {
+    null
+};
+
